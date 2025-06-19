1248c0e4bbacfa3fc6febb128349effb074535fd
Maxim <slastonm@gmail.com>
2025-06-19 20:11:26 +0300

    Fix back

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-const express = require('express'); 
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(require('./routes/auth'));
-app.use(require('./routes/gacha'));
-app.use(require('./routes/forum'));
+app.use(require("./routes/auth"));
+app.use(require("./routes/gacha"));
+app.use(require("./routes/forum"));
 
-mongoose.
-connect("mongodb://localhost:27017/my_database")
-    .then(() => {
-        console.log('MongoDB connected');
-        app.listen(5000, () => console.log('Server running on port 5000'));
-    })
-    .catch(err => console.error(err));
+// mongoose.
+// connect("mongodb://localhost:27017/my_database")
+//     .then(() => {
+//         console.log('MongoDB connected');
+//         app.listen(5000, () => console.log('Server running on port 5000'));
+//     })
+//     .catch(err => console.error(err));
+
+app.listen(5000, () => console.log("Server runing port 5000"));
