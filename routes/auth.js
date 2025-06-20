ca6094b62fb8d32cf4234b8555e135dedfffcc1e
MariaSalamatova <salamatovamary@gmail.com>
2025-06-20 11:48:04 +0300

    changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,31 @@
 const router = require('express').Router();
-const User = require('../models/User');
+//const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-router.post('/register', async (req, res) => {
+const mockUsers = [];
+
+router.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
   const hashed = await bcrypt.hash(password, 10);
   const user = new User({ username, email, password: hashed });
-  await user.save();
-  res.status(201).send('Registered');
+  mockUsers.push(user);
+  res.status(201).send("Registered");
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email });
+  const user = mockUsers.find((u) => u.email === email);
   if (!user || !(await bcrypt.compare(password, user.password))) {
-    return res.status(400).send('Invalid credentials');
+    return res.status(400).send("Invalid credentials");
   }
-  const token = jwt.sign({ id: user._id }, 'secretkey');
+  const token = jwt.sign({ email: user.email }, "secretkey");
+
   res.json({ token, user: { username: user.username } });
+});
+
+router.post("/logout", (req, res) => {
+  res.send("Logget out");
 });
 
 module.exports = router;
